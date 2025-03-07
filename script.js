006dd60679b2c01aa81987718efad3f1fb8dcc69
mltomacheski <160253044+mltomacheski@users.noreply.github.com>
2025-03-07 07:30:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var str1 =(prompt("Digite alguma coisa"));
+console.log(str1);
+
+var str2 = (prompt("Digite outra coisa "));
+console.log(str2);
+
+if (str1.length>str2.length){
+    alert("A maior string é :" + str1)
+    
+} if (str1.length<str2.length){
+    alert("A maior string é :" + str2)
+}
